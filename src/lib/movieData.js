bc1718b6bcacd1ade0606123242e1c5fb11fd4b8
Ryan Evanyo <ryan.evanyo@gmail.com>
2020-12-10 20:27:47 -0700

    Level 2

---
+++ src/lib/movieData.js
@@ -0,0 +1,15 @@
+var movieData = [
+    {title: 'Mean Girls'},
+    {title: 'Hackers'},
+    {title: 'The Grey'},
+    {title: 'Sunshine'},
+    {title: 'Ex Machina'},
+    {title: 'Feifel Goes West'},
+    {title: 'Fieifel Goes East'},
+    {title: 'Fieifel Goes North'},
+    {title: 'Fieifel Gets Laid Off From Work'}
+
+  ]
+
+
+  export default movieData;
